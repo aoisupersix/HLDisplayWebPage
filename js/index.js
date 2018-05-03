6748fedb19e2ff040a1a15cbe4f69a3473de86bf
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-05-03 11:05:19 +0900

    [update] カードが上手く収まるようにレイアウトを改良

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 var updateTime = 2000 //更新間隔[ms]
 
 $(function() {
-  for(var i = 0; i < 14; i++){
-    addCard("Card-" + i, "外出", "bg-secondary", "コメント")
+  for(var i = 1; i <= 14; i++){
+    addCard("Card-" + i, "外出", "bg-secondary")
   }
 })
 
@@ -15,13 +15,12 @@ function init() {
 /*
  * Htmlにカードを追加します
  */
-function addCard(name, statusText, color, comment){
+function addCard(name, statusText, color){
   $('#memberStatus').append(
-    $('<div class="card" style="margin: 5pt;width: 10rem;height: 10rem;"></div>').addClass(color)
+    $('<div class="card" style="margin: 5pt;width: 11rem;height: 9rem;"></div>').addClass(color)
     .append($('<h4 class="card-header"></h4>').text(name))
     .append($('<div class="card-body"></div>')
       .append($('<h1 class="card-title" style="font-weight: bold;"></h1>').text(statusText))
-      .append($('<p class="card-text"></p>').text(comment))
     )
   );
 }
