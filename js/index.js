2f37e6d9cfefce40efe5d640e3984d15fadb026b
aoisupersix <aoi_supersix@yahoo.co.jp>
2018-05-03 10:48:35 +0900

    [update] カードのデザインを改良

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ var updateTime = 2000 //更新間隔[ms]
 
 $(function() {
   for(var i = 0; i < 14; i++){
-    addCard("CardName-" + i, "外出", "コメント")
+    addCard("Card-" + i, "外出", "bg-secondary", "コメント")
   }
 })
 
@@ -15,12 +15,12 @@ function init() {
 /*
  * Htmlにカードを追加します
  */
-function addCard(name, statusText, comment){
+function addCard(name, statusText, color, comment){
   $('#memberStatus').append(
-    $('<div class="card text-white bg-success col-xs-1" style="margin: 10pt;width: 13rem;"></div>')
+    $('<div class="card" style="margin: 5pt;width: 10rem;height: 10rem;"></div>').addClass(color)
     .append($('<h4 class="card-header"></h4>').text(name))
     .append($('<div class="card-body"></div>')
-      .append($('<h1 class="card-title"></h1>').text(statusText))
+      .append($('<h1 class="card-title" style="font-weight: bold;"></h1>').text(statusText))
       .append($('<p class="card-text"></p>').text(comment))
     )
   );
